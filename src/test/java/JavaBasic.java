2674ff0afb023e02af193c3c452a11ef1607df2b
Tatyana Kuznetsova <tatyana.kuznetsova@sunfinance.group>
2019-02-03 19:55:56 +0200

    Initial commit

---
+++ src/test/java/JavaBasic.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JavaBasic {
+
+    @Test
+    public void javaBasic() {
+        int a = 13;
+        int b = 10;
+        int c = sumDigits(13, 10);
+        int d = sumDigits(a, b);
+
+        System.out.println("Summ is : " + c);
+        System.out.println("Summ is : " + d);
+        Assertions.assertEquals(23, c, "Summ is not correct ");
+    }
+
+    private Integer sumDigits(int a, int b) {
+        return a + b;
+    }
+}
